b78daa0745824f6bbb9c6d0f77486134d5abe7ae
Pastre-Sensei <zekk.spambox.kimimaro@gmail.com>
2015-12-10 11:21:21 +0100

    [Grid2] Debuggage quelque peu, pas encore au point niveau exécution

--- grid2.ml
+++ grid2.ml
@@ -7,7 +7,7 @@ type word = {
 
 let fic_ouvre_toi = fun file_path ->
   try
-    open_in file_path
+    open_in file_path;
   with exc ->
     Printf.printf "%s ne s ouvre pas en lecture\n" file_path;
     raise exc;;
@@ -20,7 +20,7 @@ let close_file = fun channel ->
     raise exc;;
 
 
-let read_grid = fun file_path ->
+let read_grid = fun file_path -> (* Genere la matrice à partir du fichier lu *)
   let file = fic_ouvre_toi file_path in
   let liste_lignes = ref [] in
   let compteur = ref 0 in
@@ -29,6 +29,7 @@ let read_grid = fun file_path ->
     if line <> "" then
       begin
         liste_lignes := line :: !liste_lignes;
+        Printf.printf "%s\n" line;
         incr compteur;
         lecture ()
       end
@@ -42,30 +43,32 @@ let read_grid = fun file_path ->
       End_of_file -> close_file file;
   end;
   liste_lignes := List.rev !liste_lignes;
-  
   let matrice_grid = Array.of_list !liste_lignes in
   matrice_grid;;
   
         
 let gen_tab_words = fun matrice -> (* Genere le tableau de mots *)
-
+  Printf.printf "Gen_tab_words appelé\n";
+  let i_length = Array.length matrice -1 in
   let compteur = ref 0 in
   let word_list = ref [] in
-  for i=0 to Array.length matrice (* Trouve les mots horizontaux *)
+  for i=0 to i_length (* Trouve les mots horizontaux *)
   do
-    for j=0 to String.length matrice.(i)
+    let j_length = String.length matrice.(i) -1 in
+    for j=0 to j_length
     do 
       let line = matrice.(i) in
       let char = line.[j] in
       if char = '_' then 
-        if j = String.length line && !compteur >=2 then  (* Gere les blancs de fin de ligne *)
-          begin
-            let new_word = {sens="horizontal"; ligne_colonne=i; debut=(j - !compteur); longueur= (!compteur)} in
-            word_list := new_word::!word_list;
-            compteur := 0;
-          end
-        else incr compteur (* Si ce n'est pas en fin de ligne, on incrémente le compteur de l'eventuel mot trouvé *)
-        
+        begin
+          incr compteur;
+          if j = j_length && !compteur >=2 then  (* Gere les blancs de fin de ligne *)
+            begin
+              let new_word = {sens="horizontal"; ligne_colonne=i; debut=(j - !compteur); longueur= (!compteur)} in
+              word_list := new_word::!word_list;
+              compteur := 0;
+            end; (* Si ce n'est pas en fin de ligne, on incrémente le compteur de l'eventuel mot trouvé *)
+        end        
       else
         if char = '*' then
           if !compteur >=2 then (* Si lorsque l'on trouve une étoile le compteur est d'au moins 2, on cree un mot *)
@@ -78,20 +81,22 @@ let gen_tab_words = fun matrice -> (* Genere le tableau de mots *)
             compteur := 0
     done
   done;
-  let longueur_ligne = String.length matrice.(0) in
-  for j=0 to longueur_ligne
+  let j_length = String.length matrice.(0) - 1 in (* Arbitraire pour l'istant *)
+  for j=0 to j_length
   do
-    for i=0 to Array.length matrice
+    for i=0 to i_length
     do
       let char = matrice.(i).[j] in
-      if char = '_' then 
-        if i = Array.length matrice && !compteur >=2 then  (* Gere les blancs de fin de colomne *)
+      if char = '_' then
+        begin
+          incr compteur;
+          if i = i_length && !compteur >=2 then  (* Gere les blancs de fin de colomne *)
           begin
             let new_word = {sens="vertical"; ligne_colonne=j; debut=(i - !compteur); longueur=(!compteur)} in
             word_list := new_word::!word_list;
             compteur := 0;
-          end
-        else incr compteur (* Si ce n'est pas en fin de ligne, on incrémente le compteur de l'eventuel mot trouvé *)
+          end;(* Si ce n'est pas en fin de ligne, on incrémente le compteur de l'eventuel mot trouvé *)
+        end
         
       else
         if char = '*' then
@@ -108,4 +113,17 @@ let gen_tab_words = fun matrice -> (* Genere le tableau de mots *)
   
   let word_table = Array.of_list !word_list in
   word_table;;
+  
+
+let print_tab_words = fun tab_words ->
+  Array.iteri (fun i word -> Printf.printf "Mot no %d : {sens : %s; ligne_col : %d; debut : %d; longueur : %d}\n" i word.sens word.ligne_colonne word.debut word.longueur) tab_words;;
+
+
+let main = fun nom_fichier ->
+  let matrice = read_grid nom_fichier in
+  let tab_words = gen_tab_words matrice in
+  print_tab_words tab_words;
+  tab_words;;
+
+main "grille_test.txt";;
   
